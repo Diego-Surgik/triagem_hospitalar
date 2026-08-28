2cec802fd96f341e070caa56593c6b30bee257fd
Diego-Surgik <164073933+Diego-Surgik@users.noreply.github.com>
2026-08-28 10:51:29 -0300

    Update triagem.py

--- triagem.py
+++ triagem.py
@@ -1,9 +1,16 @@
+pacientes_atendidos =[]
 plantao = "sim"
 while plantao == "sim":
     nome_do_paciente = input("Digite o nome do paciente:") .title() .strip()
     idade = int(input("Digite a idade do paciente:")) 
     dor = int(input("Digite o nível de dor do paciente (0 a 10): "))
-    plantao = ("aberto")
+
+    paciente_atual ={
+     "nome" : nome_do_paciente,
+     "idade" : idade,
+        "dor" : dor
+    }
+    pacientes_atendidos.append(paciente_atual)
     print(f"Paciente: {nome_do_paciente} idade: {idade} anos")
 
 
@@ -17,7 +24,12 @@ while plantao == "sim":
 
     plantao = input("Deseja registrar novo paciente? (sim/não)") .lower() .strip()
 
+print("\n--- RELATÓRIO FINAL DO PLANTÃO ---")
 
-
-
-
+for paciente in pacientes_atendidos:
+    
+    nome = paciente["nome"]
+    idade = paciente["idade"]
+    dor = paciente["dor"]
+    
+    print(f"- Paciente: {nome} | Idade: {idade} | Nível de Dor: {dor}")
